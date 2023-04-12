c89fbdf9832a5b24be6f9a1d1b240c0c26f7ff0c
Your Name <you@example.com>
2023-04-12 22:30:18 +0400

    Algoritm3_all

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 import static java.util.stream.Stream.iterate;
 
@@ -23,45 +24,31 @@ public class Main {
         ListNode node = new ListNode(1, new ListNode(2, new ListNode(2, new ListNode(4)))); // 1 -> 2 -> 2 -> 4
         System.out.println(size(node)); // 4
         asString(node); // [1 -> 2 -> 2 -> 4]
-//        asString(node); // [1 -> 2 -> 2 -> 4]
-//        ListNode third = new ListNode(3);
-//        ListNode second = insertFirst(third,2);
-//        ListNode first = insertFirst(second,1);
-//        third.next = first; //Замыкание списиска
-//        iterate(first);
-//        ListNode head = insertFirst(first,0);
-//        iterate(head);
-//        insertLast(head, 4);
-//        iterate(head);
+        System.out.println(getByIndex(node, 0)); // 1
+        System.out.println(getByIndex(node, 3)); // 4
+        System.out.println(getByIndex(node, 4)); // new IndexOutOfBoundsException
 
-//        ListNode firs = new ListNode(1, new ListNode(2,new ListNode(3)));
-//        ListNode sec = new ListNode(1, new ListNode(2,new ListNode(2,new ListNode(4))));
-//        ListNode merge = merge(firs,sec);
-//        iterate(merge);
-//        System.out.println(merge);
     }
-
-//    private static void iterate(ListNode merge) {
-//    }
-
-/**
- * Написать строковое представление списка в формате
- * [first_value -> second_value -> ... -> last_value]
- */
+    /**
+     * Написать строковое представление списка в формате
+     * [first_value -> second_value -> ... -> last_value]
+     */
     static String asString(ListNode head) {
         ListNode iterat = head;
-        StringBuilder builder =new StringBuilder();
-        while (iterat != null){
+        StringBuilder builder = new StringBuilder();
+        while (iterat != null) {
             builder.append(iterat.value);
             iterat = iterat.next;
-            if(iterat==null){
+            if (iterat == null) {
                 break;
-            }else
-            builder.append("->");
-        }String result = builder.toString();
-        System.out.print("["+result+"]");
+            } else
+                builder.append("->");
+        }
+        String result = builder.toString();
+        System.out.println("[" + result + "]");
         return null;
     }
+
     /**
      * Посчитать размер списка.
      */
@@ -74,68 +61,20 @@ public class Main {
         }
         return count;
     }
+
     /**
-     *
-     * Распечатать все элементы связанного списка
+     * Найти значение по индексу
      */
-//    static  void  iterate(ListNode node){
-//        ListNode iter = node;// [1,->2}
-//        while (iter != null) {
-//            System.out.print(iter.value +"->");
-//            iter = iter.next;
-//        }
-//        System.out.println("null");
-//    }
-    /**
-     * Создать список, полученный из head прибавлением value в начало
-     */
-//    static ListNode insertFirst(ListNode head, int value){
-//        return new ListNode(value,head);
-//    }
-    /**
-     * добавляем к существующему в конец значение value
-     */
-//    static void  insertLast(ListNode head, int value){
-//        ListNode last = new ListNode (value);
-//        ListNode iter = head;
-//        while (iter.next != null){
-//            iter = iter.next;
-//        }
-//        iter.next = last;
-//    }
-    /**
-     * Даны два отсортированных связанных списка
-     * надо их смержить и получить новый отсортированный связанный список
-     * (1,2,3),(1,2,2,4)->(1,1,2,2,2,3,4)
-     */
-//    static ListNode merge(ListNode first, ListNode second) {
-//        ListNode head = null; // 1 -> 1 -> 2
-//        ListNode iterator = null; // 2
-//        while (first != null || second != null) {
-//            int nextValue = -1; // 2
-//
-//            if (first == null) { // second != null
-//                nextValue = second.value;
-//                second = second.next;
-//            } else if (second == null) {
-//                nextValue = first.value;
-//                first = first.next;
-//            } else if (first.value > second.value) {
-//                nextValue = second.value;
-//                second = second.next;
-//            } else { // if (first.value <= second.value) {
-//                nextValue = first.value;
-//                first = first.next;
-//            }
-//
-//            if (head == null) {
-//                head = new ListNode(nextValue);
-//                iterator = head;
-//            } else {
-//                iterator.next = new ListNode(nextValue);
-//                iterator = iterator.next;
-//            }
-//        }
-//        return head;
-//    }
+    static int getByIndex(ListNode head, int index) {
+        if (head != null) {
+            ListNode iter = head;
+            int count = 0;
+            while (count != index) {
+                iter = iter.next;
+                count++;
+            }
+            return iter.value;
+        }
+        return index;
+    }
 }
